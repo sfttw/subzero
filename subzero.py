9a18f9609fae36f854076fbd85e7805196b6d010
e7z <e7z@e7zzz.com>
2017-07-01 02:45:52 -0700

    Version 2.4

--- subzero.py
+++ subzero.py
@@ -1,38 +1,37 @@
 #!/usr/bin/env python3
-'''subdomain bruteforcer. uses dns.resolver to check for results. 
+# -*- encoding: utf-8 -*-
+
+'''subdomain bruteforcer. uses dns.resolver + dig to check for results. 
 
 i kept getting too many false positives using other scripts so i made my own
 
 --elv'''
-import dns.resolver #import the module
+import dns.resolver 
 import argparse
+import threading
 
 global wordlist
 global wordlist_size
-global domain
-global log
-global logfile
 global count
-global verbose
-import threading
+global args
 
 log = []
 
 def check(target):
-    myResolver = dns.resolver.Resolver() #create a new instance named 'myResolver'
+    myResolver = dns.resolver.Resolver() 
     try:
-        myAnswers = myResolver.query(target, "A") #Lookup the 'A' record(s) for google.com
-        for rdata in myAnswers: #for each response
+        myAnswers = myResolver.query(target, "A") 
+        for rdata in myAnswers: 
             answer = target + ' ' + rdata.to_text()
-            if verbose: print('\r[+] '+answer)
+            if args['verbose']: print('\r+ '+answer)
             log.append(answer)
     except:
         pass
     try:
-        myAnswers = myResolver.query(target, "CNAME") #Lookup the 'A' record(s) for google.com
-        for rdata in myAnswers: #for each response
+        myAnswers = myResolver.query(target, "CNAME") 
+        for rdata in myAnswers: 
             answer = target + ' ' + rdata.to_text()
-            if verbose: print('\r[+] '+answer)
+            if args['verbose']: print('\r+ '+answer)
             log.append(answer)
     except: 
         pass    
@@ -40,11 +39,10 @@ def check(target):
 def main():
     global count
     while len(wordlist) > 0:
-        target = wordlist.pop() + '.' + domain
+        target = wordlist.pop() + '.' + args['domain']
         check(target)
         count += 1
         print('\rComplete: {} / {}'.format(count, wordlist_size), end='')
-        #print('\r {}'.format(target), end='')
 
 
     
@@ -60,17 +58,32 @@ if __name__ == "__main__":
     args = vars(parser.parse_args())
     threads = []
     
+    header = '''      
+            ,gg,                                                                     
+           i8""8i               ,dPYb,                                               
+           `8,,8'               IP'`Yb                                               
+            `88'                I8  8I                                               
+            dP"8,               I8  8'                                               
+           dP' `8a  gg      gg  I8 dP        ,gggg,   ,ggg,    ,gggggg,    ,ggggg,   
+          dP'   `Yb I8      8I  I8dP   88gg d8"  Yb  i8" "8i   dP""""8I   dP"  "Y8ggg
+      _ ,dP'     I8 I8,    ,8I  I8P    8I  dP    dP  I8, ,8I  ,8'    8I  i8'    ,8I  
+      "888,,____,dP,d8b,  ,d8b,,d8b,  ,8I,dP  ,adP'  `YbadP' ,dP     Y8,,d8,   ,d8'  
+      a8P"Y88888P" 8P'"Y88P"`Y88P'"Y88P"'8"   ""Y8d8888P"Y8888P      `Y8P"Y8888P"    
+                                               ,d8I'                                 
+                                             ,dP'8I                                  
+                                            ,8"  8I                                  
+                                            I8   8I                                  
+                                            `8, ,8I                                  
+                                             `Y8P"                                   \n'''
+    print(header)
     if args['domain']:
-        verbose = args['verbose']
         wordlist = open(args['wordlist'], 'r').readlines()
         wordlist = [i.strip() for i in wordlist]
         wordlist.reverse()
         wordlist_size = len(wordlist)
-        domain = args['domain']
         count = 0
-        logfile = args['output']
         threadcount = int(args['threadcount'])
-        logfile = open(logfile,'a')
+        logfile = open(args['output'],'w')
 
         
         for i in range(threadcount):
@@ -88,6 +101,7 @@ if __name__ == "__main__":
         if count < wordlist_size: pass
         else:
             if args['output']:
-                for i in log: logfile.write(i)
+                for i in log: 
+                    logfile.write(i+'\n')
             print('\r\n')
             break
