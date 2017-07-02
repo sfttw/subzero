b0e20fa13a4eee7e72a53fa9ddebbd2f7ae3e5e8
e7z <e7z@e7zzz.com>
2017-07-01 19:26:33 -0700

    scan.py added. works but not ready for primetime

---
+++ scan.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# -*- encoding: utf-8 -*-
+
+import argparse
+import socket
+import threading
+count = 0
+global args
+global domain
+from contextlib import closing
+ports = []
+log = []
+ports.extend(range(65536))
+ports.reverse()
+def check(host, port):
+    with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as sock:
+        try:sock.settimeout(args['timeout'])
+        except:sock.settimeout(.3)
+        if sock.connect_ex((host, port)) == 0:
+            print('\nOpen: '+str(port))
+            log.append(str(port))
+        else:
+            pass
+
+
+
+
+
+def main(target='77night.club'):
+    try:target=args['domain']
+    except:pass
+    global count
+    while len(ports) > 0:
+        check(target, ports.pop())
+        count += 1
+        print('\rComplete: {} / {}'.format(count, 65535), end='')
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='scan.py')
+    parser.add_argument('domain', help='target to scan, like domain.com')
+    parser.add_argument('-p','--ports', help='Ports (0-65355)', default='0-65355', required=False)
+    parser.add_argument('-v','--verbose', help='Verbose', required=False, action='store_true')
+    parser.add_argument('-o','--output', help='Output file', required=False, default='log.txt')
+    parser.add_argument('-t','--threadcount', help='Number of Threads', required=False, default=50, type=int)    
+    parser.add_argument('-to','--timeout', help='socket timeout', required=False, default=.3)
+    threads =[]
+    args = vars(parser.parse_args())
+    for i in range(50):
+        t = threading.Thread(target=main)
+        threads.append(t)
+        t.start()  
+    
+    
--- subzero.py
+++ subzero.py
@@ -1,21 +1,19 @@
 #!/usr/bin/env python3
 # -*- encoding: utf-8 -*-
+'''subdomain bruteforcer. uses dns.resolver + dig to check for results. '''
+__author__ = 'e7v'
+__version__ = '2.4: Clint Eastwood'
 
-'''subdomain bruteforcer. uses dns.resolver + dig to check for results. 
-
-i kept getting too many false positives using other scripts so i made my own
-
---elv'''
 import dns.resolver 
 import argparse
 import threading
 
-global wordlist
-global wordlist_size
-global count
+#global variables
 global args
-
 log = []
+count = 0
+wordlist = []
+wordlist_size = 0 
 
 def check(target):
     myResolver = dns.resolver.Resolver() 
@@ -23,7 +21,7 @@ def check(target):
         myAnswers = myResolver.query(target, "A") 
         for rdata in myAnswers: 
             answer = target + ' ' + rdata.to_text()
-            if args['verbose']: print('\r+ '+answer)
+            if args['verbose']: print('\r'+answer+'\n',end='')
             log.append(answer)
     except:
         pass
@@ -31,7 +29,7 @@ def check(target):
         myAnswers = myResolver.query(target, "CNAME") 
         for rdata in myAnswers: 
             answer = target + ' ' + rdata.to_text()
-            if args['verbose']: print('\r+ '+answer)
+            if args['verbose']: print('\r'+answer+'\n',end='')
             log.append(answer)
     except: 
         pass    
@@ -43,22 +41,19 @@ def main():
         check(target)
         count += 1
         print('\rComplete: {} / {}'.format(count, wordlist_size), end='')
-
-
-    
     
 if __name__ == "__main__": 
     parser = argparse.ArgumentParser(description='Subzero - subdomain bruteforcer')
     parser.add_argument('-d','--domain', help='Domain', required=True)
-    parser.add_argument('-w','--wordlist', help='Wordlist', default='subdomains-10000.txt', required=False)
+    parser.add_argument('-w','--wordlist', help='Wordlist', default='subdomains.txt', required=False)
     parser.add_argument('-v','--verbose', help='Verbose', required=False, action='store_true')
     parser.add_argument('-o','--output', help='Output file', required=False, default='log.txt')
-    parser.add_argument('-t','--threadcount', help='Number of Threads', required=False, default=50)    
+    parser.add_argument('-t','--threadcount', help='Number of Threads', required=False, default=100)    
     
     args = vars(parser.parse_args())
     threads = []
     
-    header = '''      
+    header = '''\033[94m      
             ,gg,                                                                     
            i8""8i               ,dPYb,                                               
            `8,,8'               IP'`Yb                                               
@@ -69,19 +64,18 @@ if __name__ == "__main__":
       _ ,dP'     I8 I8,    ,8I  I8P    8I  dP    dP  I8, ,8I  ,8'    8I  i8'    ,8I  
       "888,,____,dP,d8b,  ,d8b,,d8b,  ,8I,dP  ,adP'  `YbadP' ,dP     Y8,,d8,   ,d8'  
       a8P"Y88888P" 8P'"Y88P"`Y88P'"Y88P"'8"   ""Y8d8888P"Y8888P      `Y8P"Y8888P"    
-                                               ,d8I'                                 
+                                               ,d8I'         fastest in the west                           
                                              ,dP'8I                                  
                                             ,8"  8I                                  
                                             I8   8I                                  
                                             `8, ,8I                                  
-                                             `Y8P"                                   \n'''
+                                             `Y8P"                                   \033[0m\n'''
     print(header)
     if args['domain']:
         wordlist = open(args['wordlist'], 'r').readlines()
         wordlist = [i.strip() for i in wordlist]
         wordlist.reverse()
         wordlist_size = len(wordlist)
-        count = 0
         threadcount = int(args['threadcount'])
         logfile = open(args['output'],'w')
 
